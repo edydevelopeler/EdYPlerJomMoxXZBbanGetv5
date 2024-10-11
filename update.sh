4d56f0ba81e514c7afc4c370cb31a64340a76246
Edy Dev <152673375+edydevelopeler@users.noreply.github.com>
2024-10-11 14:34:56 +0700

    ngentot lu

--- update.sh
+++ update.sh
@@ -27,7 +27,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/BjhonStore/Autoscript-tunneling-premium/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/edydevelopeler/EdYPlerJomMoxXZBbanGetv5/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
